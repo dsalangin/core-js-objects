1dfa9698a277dd8838daa7414e039d817fd33e8a
dmitriy.salangin <dmitriy.salangin@simpleone.ru>
2024-03-11 23:39:18 +0300

    feat: resolve removeProperties

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 function mergeObjects(objects) {
-  const result = {};
-  objects.forEach((obj) => {
-    Object.entries(obj).forEach(([key, value]) => {
-      console.log('присвоение', result[key]);
-      if (!result[key]) {
-        result[key] = value;
-      } else {
-        result[key] += value;
-      }
-    });
-  });
-  return result;
+  return [...objects];
 }
 
-console.log(
-  mergeObjects([
-    { a: 1, b: 2 },
-    { b: 3, c: 5 },
-  ])
-);
+console.log(mergeObjects(['asdf']));
--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -60,9 +60,14 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const copyObj = { ...obj };
+  keys.forEach((key) => {
+    delete copyObj[key];
+  });
+  return copyObj;
 }
+// throw new Error('Not implemented');
 
 /**
  * Compares two source objects. Returns true if the objects are equal and false otherwise.
